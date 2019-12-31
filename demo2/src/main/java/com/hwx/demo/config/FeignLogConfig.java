a394368704499cec7f355ab551db5eb1c9f836dd
hwx <2373682019@qq.com>
2019-12-31 13:59:13 +0800

    feign日志配置

--- demo2/src/main/java/com/hwx/demo/config/FeignLogConfig.java
+++ demo2/src/main/java/com/hwx/demo/config/FeignLogConfig.java
@@ -6,12 +6,12 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * 为feign客户端配置日志
- *  也可使用配置文件为feign配置日志
+ * 也可使用配置文件为feign配置日志
  */
 @Configuration
 public class FeignLogConfig {
     @Bean
-    Logger.Level feignLoggerLevel(){
-        return Logger.Level.HEADERS;
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 }
--- demo2/src/main/java/com/hwx/demo/feign/Demo1Client.java
+++ demo2/src/main/java/com/hwx/demo/feign/Demo1Client.java
@@ -1,5 +1,6 @@
 package com.hwx.demo.feign;
 
+import com.hwx.demo.config.FeignLogConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
